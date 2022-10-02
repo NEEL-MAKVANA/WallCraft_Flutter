d6e5db886188c61b65dc932de46ac14114ba0b20
MAKVANA NEEL <neelmakvana168@gmail.com>
2022-10-02 10:35:15 +0530

    Update main.dart

--- wallcraft/lib/main.dart
+++ wallcraft/lib/main.dart
@@ -12,7 +12,7 @@ class WallCraft extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WallCraft',
+      title: 'Wallyfy',
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
