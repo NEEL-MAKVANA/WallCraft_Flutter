651fe4b1bff22b7baedf1375ea043d495f2947b1
Dev12122002 <devoza121220@gmail.com>
2022-10-19 01:22:54 +0530

    final project

--- wallcraft/lib/views/category.dart
+++ wallcraft/lib/views/category.dart
@@ -19,6 +19,19 @@ class _CategorieState extends State<Categorie> {
 
   List<WallpaperModel> wallpapers = [];
 
+
+  void initializeWallpapers(){
+    for(int i=0; i<80; i++){
+      SrcModel srcModel = new SrcModel();
+      // https://miro.medium.com/max/1080/0*DqHGYPBA-ANwsma2.gif
+      // srcModel.portrait = 'assets/wallLoad.gif';
+      WallpaperModel wallpaperModel = new WallpaperModel(src: srcModel);
+      // wallpaperModel = WallpaperModel.fromMap(element);
+      wallpaperModel.avg_color = "#ffffff";
+      wallpapers.add(wallpaperModel);
+    }
+  }
+
   getSearchWallpapers(String query) async {
     var response = await http.get(
         Uri.parse("https://api.pexels.com/v1/search?query=$query&per_page=80"),
@@ -27,13 +40,16 @@ class _CategorieState extends State<Categorie> {
     // print(response.body.toString());
 
     Map<String, dynamic> jsonData = jsonDecode(response.body);
+    var i=0;
     jsonData["photos"].forEach((element) {
       // print(element);
       SrcModel srcModel = new SrcModel();
       WallpaperModel wallpaperModel = new WallpaperModel(src: srcModel);
       wallpaperModel = WallpaperModel.fromMap(element);
       wallpaperModel.avg_color = element["avg_color"];
-      wallpapers.add(wallpaperModel);
+      // wallpapers.add(wallpaperModel);
+      wallpapers[i] = wallpaperModel;
+      i++;
     });
 
     setState(() {});
@@ -42,6 +58,7 @@ class _CategorieState extends State<Categorie> {
   @override
   void initState() {
     // TODO: implement initState
+    initializeWallpapers();
     getSearchWallpapers(widget.categorieName);
     super.initState();
   }
@@ -49,6 +66,7 @@ class _CategorieState extends State<Categorie> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: brandName(MediaQuery.of(context).size.width,
             MediaQuery.of(context).size.height),
